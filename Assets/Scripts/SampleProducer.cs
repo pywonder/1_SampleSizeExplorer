5ccc775cc1ea4bdfc39edc0f4cc7fc4822542847
Yongwoo Park <pywonder@gmail.com>
2024-04-22 23:41:48 -0400

    2024-04-22_Progress

--- Assets/Scripts/SampleProducer.cs
+++ Assets/Scripts/SampleProducer.cs
@@ -9,7 +9,13 @@ public class SampleProducer : MonoBehaviour
     [SerializeField] private GameObject _metal;
     [SerializeField] private List<Material> _metalFinishOptions;
 
+    [SerializeField] private GameObject _wood;
+    [SerializeField] private List<Material> _woodFinishOptions;
 
+    [SerializeField] private GameObject _Desk1;
+    [SerializeField] private GameObject _Desk2;
+    [SerializeField] private List<Material> _deskFinishOptions;
+    
     public void TileOptionChange(int index)
     {
         foreach (var opt in _tileOptions)
@@ -32,4 +38,43 @@ public class SampleProducer : MonoBehaviour
             }
         }
     }
+    public void WoodMaterialChange(int index)
+    {
+        if (index >= 0 && index < _metalFinishOptions.Count)
+        {
+            Renderer metalRenderer = _wood.GetComponent<Renderer>();
+
+            if (metalRenderer != null)
+            {
+                metalRenderer.material = _woodFinishOptions[index];
+            }
+        }
+    }
+    
+    public void Desk1MaterialChange(int index)
+    {
+        if (index >= 0 && index < _metalFinishOptions.Count)
+        {
+            Renderer metalRenderer = _Desk1.GetComponent<Renderer>();
+
+            if (metalRenderer != null)
+            {
+                metalRenderer.material = _deskFinishOptions[index];
+            }
+        }
+    }
+    
+    public void Desk2MaterialChange(int index)
+    {
+        if (index >= 0 && index < _metalFinishOptions.Count)
+        {
+            Renderer metalRenderer = _Desk2.GetComponent<Renderer>();
+
+            if (metalRenderer != null)
+            {
+                metalRenderer.material = _deskFinishOptions[index];
+            }
+        }
+    }
+    
 }
